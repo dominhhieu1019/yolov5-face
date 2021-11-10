3c39eb87cffefea39fc0f2c0657a5163a03769f7
Do Minh Hieu <dominhhieu1019@gmail.com>
2021-11-10 23:29:00 +0700

    ok

--- models/common.py
+++ models/common.py
@@ -171,7 +171,6 @@ class SPP(nn.Module):
         return self.cv2(torch.cat([x] + [m(x) for m in self.m], 1))
 
 
-
 class SPPF(nn.Module):
     # Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher
     def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
@@ -187,7 +186,9 @@ class SPPF(nn.Module):
             warnings.simplefilter('ignore')  # suppress torch 1.9.0 max_pool2d() warning
             y1 = self.m(x)
             y2 = self.m(y1)
+           
             return self.cv2(torch.cat([x, y1, y2, self.m(y2)], 1))
+
 
 class Focus(nn.Module):
     # Focus wh information into c-space
