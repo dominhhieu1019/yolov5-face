58576f67d5661d3fe9014e0806abe04c9c78ce44
Do Minh Hieu <dominhhieu1019@gmail.com>
2021-11-19 17:04:42 +0700

    ok

--- torch2tensorrt/main.py
+++ torch2tensorrt/main.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     device="cuda:0" 
     # onnx_model_path=cur_path+"/../../yolov5l-face.onnx" #ONNX模型路径 
     # onnx_model_path=cur_path+"/../weights/yolov5n.onnx"
-    trt_engine_path=cur_path+"/../weights/yolov5n-face.trt"
+    trt_engine_path=cur_path+"/../weights/yolov5n-0.5.trt"
 
     fp16_mode=True  #True则FP16推理
 
